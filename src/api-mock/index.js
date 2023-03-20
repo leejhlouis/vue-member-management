d2e82e8794fe3c060b0ed254382f682e03981bf0
leejhlouis <louisgustavo07@gmail.com>
2023-03-20 15:57:12 +0700

    fix: revert back to 100ms for api mock's delay response

--- src/api-mock/index.js
+++ src/api-mock/index.js
@@ -12,7 +12,7 @@ const methodsMap = {
   DELETE: 'onAny',
 };
 
-const delayResponse = 500;
+const delayResponse = 100;
 const mock = new MockAdapter(axios, { delayResponse });
 
 const routes = [...members, ...addresses, ...regions];
